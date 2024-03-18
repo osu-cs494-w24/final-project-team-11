25939be7497f01d3d9fa8ba664678f2f523a3fd7
Quinn <91570904+qnnrb@users.noreply.github.com>
2024-03-18 00:09:39 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import {  Outlet } from 'react-router-dom'
+import styled from '@emotion/styled'
+import eventsData from '../../public/EventDummy.json'
+import Event from '../components/Event'
 
+const EventsContainer = styled.div`
+
+`
 
 export function Home() {
     return (
         <>
-            <p id="hometext">This is the home page, click on a section to get started!</p>
-           
+            <p id="hometext">Today's Events</p>
+            <EventsContainer>
+                {eventsData.map(e => (<Event event={e} />)
+                )}
+            </EventsContainer>
             <main><Outlet /> </main>
         </>
     )
