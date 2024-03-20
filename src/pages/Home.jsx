54ab61e032f00a0b879d0f641eadff2d59c5d84b
Quinn <91570904+qnnrb@users.noreply.github.com>
2024-03-20 00:48:15 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,40 @@
-import { Outlet } from 'react-router-dom';
+import {  Outlet } from 'react-router-dom'
+import styled from '@emotion/styled'
+import Event from '../components/Event'
+import { useQuery } from '@tanstack/react-query'
+import { useState, useEffect } from 'react'
+import Spinner from '../components/Spinner'
+
+const EventsContainer = styled.div`
+
+`
 
 export function Home() {
-  return (
-    <>
-      <p id="hometext">This is the home page, click on a section to get started!</p>
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
+
+    const sports_query = ''
+    const { fetchStatus, isLoading, error, data } = useQuery({
+        queryKey: ["getSports", sports_query],
+        queryFn: async () => {
+            console.log('== query function called');
+            const res = await fetch(
+                ' https://api.the-odds-api.com/v4/sports/basketball_ncaab/odds/?apiKey=1bb1eee28190f9ab8f91d6d9194a7f73&regions=us&markets=h2h,spreads&oddsFormat=american'
+            );
+            const jsonData = await res.json();
+            console.log("== API Response:", jsonData); // Log the API response
+            return jsonData;
+        }
+    }); 
+
+    console.log(" sports data in Home: ", data)
+
+    return (
+        <>
+            <p id="hometext">Today's Events</p>
+            {isLoading && <Spinner />}
+            <EventsContainer>
+                <Event data={data}/>
+            </EventsContainer>
+            <main><Outlet /> </main>
+        </>
+    )
 }
